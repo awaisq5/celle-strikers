231949e64f154f35a6c7786fdedf80b4d1665b20
Awais <awaishanifqurashi@gmail.com>
2026-04-26 03:29:05 +0200

    finalized

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Celle Strikers",
-        short_name: "Strikers",
-        description: "Sunday cricket scoring app for Celle Strikers",
-        theme_color: "#020617",
-        background_color: "#020617",
-        display: "standalone",
-        start_url: "/",
-        icons: [
-          {
-            src: "/icon-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 });
